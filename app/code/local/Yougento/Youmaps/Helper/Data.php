936aa80c4ffe8253c8c214ef9a5c1dde46a9ee43
unknown <Admin@.(none)>
2013-04-23 22:42:00 -0500

    new function

--- app/code/local/Yougento/Youmaps/Helper/Data.php
+++ app/code/local/Yougento/Youmaps/Helper/Data.php
@@ -1,5 +1,7 @@
 <?php
 class Yougento_Youmaps_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	
+	function createNewFunct{
+		echo  'i am the god of hell and fire and i bring you FIREEEEEE!!';
+	}
 }
